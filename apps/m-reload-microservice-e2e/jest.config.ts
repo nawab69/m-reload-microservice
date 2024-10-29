07b178f624d559918b1b25bf6f15581c963463f9
Nawab Khairuzzaman <nawabkhairuzzaman@gmail.com>
2024-10-29 06:58:43 +0600

    Initial commit

---
+++ apps/m-reload-microservice-e2e/jest.config.ts
@@ -0,0 +1,15 @@
+export default {
+  displayName: 'm-reload-microservice-e2e',
+  preset: '../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/m-reload-microservice-e2e',
+};
